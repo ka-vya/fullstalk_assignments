bdd7b5e98fd211bdcd9d6aaf05707ff45ed4a54d
kavya <kavyakmarar1997@gmail.com>
2021-10-06 13:32:43 +0530

    operators

--- app.js
+++ app.js
@@ -1,2 +1,38 @@
-var age=19
-console.log(name)
+var a=2
+var b=5
+console.log('a+b=',a+b)
+console.log('a*b=',a*b)
+console.log('a-b=',a-b)
+console.log('a/b=',a/b)
+console.log('a**b=',a**b)
+console.log('a%b=',a%b)
+
+//assignment operators
+
+console.log('b=',b)
+b+=10
+console.log('b+=10',b)
+b++
+console.log('b++=',b)
+b-=10
+console.log('b-10=',b)
+
+//comparison operators
+var d=10
+var e=2
+console.log(d>e)
+console.log(d<e)
+console.log(d==e)
+//logical operators
+
+console.log( 'true && true =',true && true)
+console.log('true && false =',true && false)
+console.log('false && true =',false && true)
+console.log('false && false =',false && false)
+
+console.log( 'true || true =',true || true)
+console.log('true || false=',true || false)
+console.log('false || true =',false || true)
+console.log('false || false =',false || false)
+
+
