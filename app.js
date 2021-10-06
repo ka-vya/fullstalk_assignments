c72be314fe0c9b5725f0774959b0b01b9cb4be34
kavya <kavyakmarar1997@gmail.com>
2021-10-06 12:49:53 +0530

    null vs undefiend

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-console.log(typeof'kavya')
-console.log(typeof "gackagkjg")
-console.log(typeof 1862639)
-console.log(typeof -2378587219)
-console.log(typeof true)
-console.log(typeof false)
-console.log(typeof'nsahcd')
-console.log(typeof 234)
-console.log(typeof'kavya')
-console.log(typeof'kavya')
+var age=19
+console.log(name)
