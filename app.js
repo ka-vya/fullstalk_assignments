b25d4ee367d675e9a12df46a9d4ff3716e0b7b1a
kavya <kavyakmarar1997@gmail.com>
2021-10-12 13:35:26 +0530

    DOM selection API

--- app.js
+++ app.js
@@ -1,38 +1,7 @@
-var a=2
-var b=5
-console.log('a+b=',a+b)
-console.log('a*b=',a*b)
-console.log('a-b=',a-b)
-console.log('a/b=',a/b)
-console.log('a**b=',a**b)
-console.log('a%b=',a%b)
+var heading=document.getElementById('heading')
+var para=document.getElementsByClassName('para')
+var tags = document.getElementsByTagName('a')
+var  query= document.querySelector('.para')
 
-//assignment operators
-
-console.log('b=',b)
-b+=10
-console.log('b+=10',b)
-b++
-console.log('b++=',b)
-b-=10
-console.log('b-10=',b)
-
-//comparison operators
-var d=10
-var e=2
-console.log(d>e)
-console.log(d<e)
-console.log(d==e)
-//logical operators
-
-console.log( 'true && true =',true && true)
-console.log('true && false =',true && false)
-console.log('false && true =',false && true)
-console.log('false && false =',false && false)
-
-console.log( 'true || true =',true || true)
-console.log('true || false=',true || false)
-console.log('false || true =',false || true)
-console.log('false || false =',false || false)
-
+console.log(query)
 
