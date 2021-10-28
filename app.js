a59a980a1547832773c6d636aefaf58574636d1e
kavya <kavyakmarar1997@gmail.com>
2021-10-28 12:08:09 +0530

    calculator

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
-var button=document.querySelector('button')
-var body=document.querySelector('body')
+const a=document.querySelectorAll('input')[0]
+const b=document.querySelectorAll('input')[1]
+const add=document.querySelector('#add')
+const multiply=document.querySelector('#multiply')
+const subtract=document.querySelector('#subtract')
+const resultbox=document.querySelector('.result')
+
+const sum = ()=>{
+
+}
+const product=()=>{
+
+}
 
 
-const callbackfunc = (event) =>{
-    body.classList.toggle('dark')
+const calculator =(event,operation)=>{
+    console.log(event.target)
+
+    
+switch (operation){
+    case "add":
+         resultbox.innerHTML = parseInt(a.value) + parseInt(b.value)
+         
+         break
+    case "multiply":
+         resultbox.innerHTML = parseInt(a.value) * parseInt(b.value)
+         break
+    case "subtract":
+         resultbox.innerHTML = parseInt(a.value) - parseInt(b.value)
+         break
+    default:
+        resultbox.innerHTML="not a valaid operation"     
     
 }
-button.addEventListener('click',callbackfunc)
+}
+
+add.addEventListener('click', (event) =>calculator(event,'add'))
+multiply.addEventListener('click',(event)=>calculator(event,'multiply'))
+subtract.addEventListener('click',(event)=>calculator(event,'subtract'))
