4b5e8446c466ce81c894862d0789085e5c346e53
kavya <kavyakmarar1997@gmail.com>
2021-10-29 20:41:19 +0530

    arrays

--- app.js
+++ app.js
@@ -1,39 +1,4 @@
-const a=document.querySelectorAll('input')[0]
-const b=document.querySelectorAll('input')[1]
-const add=document.querySelector('#add')
-const multiply=document.querySelector('#multiply')
-const subtract=document.querySelector('#subtract')
-const resultbox=document.querySelector('.result')
+var arr=[1,2,3,4,5,6,7,8]
 
-const sum = ()=>{
-
-}
-const product=()=>{
-
-}
-
-
-const calculator =(event,operation)=>{
-    console.log(event.target)
-
-    
-switch (operation){
-    case "add":
-         resultbox.innerHTML = parseInt(a.value) + parseInt(b.value)
-         
-         break
-    case "multiply":
-         resultbox.innerHTML = parseInt(a.value) * parseInt(b.value)
-         break
-    case "subtract":
-         resultbox.innerHTML = parseInt(a.value) - parseInt(b.value)
-         break
-    default:
-        resultbox.innerHTML="not a valaid operation"     
-    
-}
-}
-
-add.addEventListener('click', (event) =>calculator(event,'add'))
-multiply.addEventListener('click',(event)=>calculator(event,'multiply'))
-subtract.addEventListener('click',(event)=>calculator(event,'subtract'))
+arr.splice(5,2)
+console.log(arr)
