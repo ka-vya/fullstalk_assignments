615f59c629b824a6531a97b88f7fb38dfc108bd7
kavya <kavyakmarar1997@gmail.com>
2021-10-29 21:47:49 +0530

    todo delete

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ var button=document.querySelector('button')
 var input=document.querySelector('input')
 var list=document.querySelector('ul')
 
+var chores=[]
 
+var deleteItem =(value)=>{
+    const index=chores.indexOf(value)
+    chores.splice(index,1)
+    console.log(chores)
+}
 const callbackfunc = (event) =>{
-    const inputValue=input.value
+   //const inputValue=input.value
+   const inputValue=input.value
+   if (chores.includes(inputValue)){
+        console.log('already exist')
+   }
+   else{
+     chores.push(inputValue)
     const element=document.createElement('li')
     const textNode=document.createTextNode(input.value)
     element.appendChild(textNode)
     list.appendChild(element)
-   
+    element.addEventListener('click' ,(e) => {e.target.remove()})
+   }
 }
 button.addEventListener('click',callbackfunc)
